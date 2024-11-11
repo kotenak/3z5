b35fc484a0c4dce11f6a8cff2a8c03bc33f1daae
kotenak <128995380+kotenak@users.noreply.github.com>
2024-11-11 19:29:50 +0500

    Update info-panel.js

--- info-panel.js
+++ info-panel.js
@@ -9,19 +9,19 @@ AFRAME.registerComponent('info-panel', {
 
     this.movieInfo = {
       pic1Button: {
-        title: 'Tomie (1987))',
+        title: 'Tomie (1987) ',
         imgEl: document.querySelector('#pic1MovieImage'),
-        description: 'She subjugates men, luring them into the nets of mad love. Her name is Tomie. And her lives are countless. No matter how many times you kill her, the beautiful Tomie always returns. Ah, Tomie! The time will come when the whole world will bow before you."'
+        description: 'She subjugates men, luring them into the nets of mad love. Her name is Tomie. And her lives are countless.'
       },
       pic2Button: {
-        title: 'Soichi (2002)',
+        title: 'Soichi (2002) ',
         imgEl: document.querySelector('#pic2MovieImage'),
-        description: 'I'll remember you... I'll make your life a living nightmare!" Souichi, the youngest child of the Tsujii family, loves to walk around with nails in his mouth and cast terrible curses on people, which is why amazing and inexplicable events constantly happen around him. Welcome to the sinister and darkly humorous world of Souichi.'
+        description: 'I'll remember you... I'll make your life a living nightmare!'
       },
       pic3Button: {
-        title: ''Dead Man's Love (2011)',
+        title: ''Dead Man's Love (2011) ',
         imgEl: document.querySelector('#pic3MovieImage'),
-        description: 'A mysterious handsome man wanders around the city, suddenly appearing before girls telling fortunes at crossroads and destroying their most cherished dreams with cruel predictions. All those who are unlucky enough to meet him, for some unknown reason, commit suicide in the most cruel way. The evening city, shrouded in thick fog, is permeated with the smell of imminent death. Join the unusual madness of this story, flavored with drops of fresh blood!"'
+        description: 'A mysterious handsome man wanders around the city, suddenly appearing before girls telling fortunes at crossroads and destroying their most cherished dreams with cruel predictions.'
       }
     };
 
